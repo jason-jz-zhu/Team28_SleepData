57929026c8c8babba62820a044086801e71deb4c
jiandaoz1 <55939857+jiandaoz1@users.noreply.github.com>
2019-10-23 00:11:41 -0500

    Update read_edf_data.py

--- read_edf_data.py
+++ read_edf_data.py
@@ -18,7 +18,7 @@ Hyppath = glob.glob(edf_path+"/*Hypnogram.edf")
 print(len(PSGpath))
 print(len(Hyppath))
 my_dict = defaultdict(lambda: defaultdict(dict))
-ind = 0 
+ind = 0
 for ipsgpath in PSGpath[0:2]:
    [sub,night] = re.match(r".*"+pnamestart+"(\d\d)(\d)", ipsgpath).groups()
    ihyppath = glob.glob(edf_path+"/"+pnamestart+sub+night+"*-Hypnogram.edf")
@@ -30,23 +30,22 @@ for ipsgpath in PSGpath[0:2]:
    my_dict[ind]['Night'] = night
    num_chn = len(raw.ch_names)
    for j in range(num_chn):
-   	# pick one data point every 100 
-   		my_dict[ind][raw.ch_names[j]] = raw.get_data(j)[0][::100]
-	# add annot, divide 100 to keep consistent
-   my_dict[ind]['annot_time']= np.int32(np.round(events[:,0]/100))
-   my_dict[ind]['annot_stage']= events[:,2]
+       # pick one data point every 100
+       my_dict[ind][raw.ch_names[j]] = raw.get_data(j)[0][::100].tolist()
+    # add annot, divide 100 to keep consistent
+   my_dict[ind]['annot_time']= np.int32(np.round(events[:,0]/100)).tolist()
+   my_dict[ind]['annot_stage']= events[:,2].tolist()
    ind = ind + 1
 
 #convert dict to json
-output = pd.Series(my_dict).to_json(orient='values')
+#output = pd.Series(my_dict).to_json(orient='values')
 
 # write to json files
 with open('result.json', 'w') as fp:
-   json.dump(output, fp)
+   json.dump(my_dict, fp)
 
 
 #questions:
 #filter
 #lighoff?
 #sampling frequency the same?
-
