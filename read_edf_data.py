a99d93ec3bba86de97582f769f0e8de678f76ff6
shuyanmei <33563212+shuyanmei@users.noreply.github.com>
2019-10-24 23:04:41 -0400

    change writing to a single file to multiple json files

--- read_edf_data.py
+++ read_edf_data.py
@@ -7,45 +7,36 @@ import json
 import os
 import pandas as pd
 
-edf_path = "../data/sleep-edf-database-expanded-1.0.0/sleep-cassette"
-# edf_path = "../data/sleep-edf-database-expanded-1.0.0/sleep-telemetry"
+# edf_path = "data/sleep-edf-database-expanded-1.0.0/sleep-cassette"
+edf_path = "../data/sleep-edf-database-expanded-1.0.0/sleep-telemetry"
 
 data = defaultdict()
-# pnamestart="ST7"
-pnamestart="SC4"
+pnamestart="ST7"
+# pnamestart="SC4"
 PSGpath = glob.glob(edf_path+"/*PSG.edf")
 Hyppath = glob.glob(edf_path+"/*Hypnogram.edf")
 print(len(PSGpath))
 print(len(Hyppath))
-my_dict = defaultdict(lambda: defaultdict(dict))
-ind = 0
-for ipsgpath in PSGpath[0:2]:
+
+#write file for each subject to a single json file
+my_dict = {}
+for ipsgpath in PSGpath:
    [sub,night] = re.match(r".*"+pnamestart+"(\d\d)(\d)", ipsgpath).groups()
    ihyppath = glob.glob(edf_path+"/"+pnamestart+sub+night+"*-Hypnogram.edf")
    raw = mne.io.read_raw_edf(os.path.join(ipsgpath), preload=True, stim_channel='auto', verbose=False)
    annot = mne.read_annotations(ihyppath[0])
    raw.set_annotations(annot)
    events, event_id = mne.events_from_annotations(raw)
-   my_dict[ind]['Subject'] = sub
-   my_dict[ind]['Night'] = night
+   my_dict['Subject'] = sub
+   my_dict['Night'] = night
    num_chn = len(raw.ch_names)
    for j in range(num_chn):
        # pick one data point every 100
-       my_dict[ind][raw.ch_names[j]] = raw.get_data(j)[0][::100].tolist()
+       my_dict[raw.ch_names[j]] = raw.get_data(j)[0][::100].tolist()
     # add annot, divide 100 to keep consistent
-   my_dict[ind]['annot_time']= np.int32(np.round(events[:,0]/100)).tolist()
-   my_dict[ind]['annot_stage']= events[:,2].tolist()
-   ind = ind + 1
-
-#convert dict to json
-#output = pd.Series(my_dict).to_json(orient='values')
-
-# write to json files
-with open('result.json', 'w') as fp:
-   json.dump(my_dict, fp)
+   my_dict['annot_time']= np.int32(np.round(events[:,0]/100)).tolist()
+   my_dict['annot_stage']= events[:,2].tolist()
+   with open('data/ProcessedData/sleep-telemetry/subject_%s.json'%sub, 'w') as fp:
+    json.dump(my_dict, fp)
 
 
-#questions:
-#filter
-#lighoff?
-#sampling frequency the same?
